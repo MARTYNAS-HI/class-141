c80502e508edee2bcf4747e297975923e1d50793
MARTYNAS-HI <78751357+MARTYNAS-HI@users.noreply.github.com>
2022-03-28 16:13:58 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,65 @@
+img = "";
+noseX = 0;
+noseY = 0;
+marioX = 325;
+marioY = 325;
+
+function preload() {
+	world_start = loadSound("world_start.wav");
+	setSprites();
+	MarioAnimation();
+	img = loadImage("mario.jpg");
+}
+
+function setup() {
+	canvas = createCanvas(650,400);
+	canvas.parent('canvas');
+	instializeInSetup(mario);
+	
+	video = createCapture(VIDEO);
+	video.size(800,400);
+	video.parent('game_console');
+	
+    poseNet = ml5.poseNet(video, modelLoaded);
+	poseNet.on('pose', gotPoses);
+}
+
+function draw() {
+background("#D3D3D3");
+	game();
+	if(noseX < 300)
+	{
+		marioX = marioX - 1;
+	}
+	
+	if(noseX > 300)
+	{
+		marioX = marioX + 1;
+	}
+	
+		if(noseY < 150)
+	{
+		marioY = marioY - 1;
+	}
+	
+image(img,marioX, marioY, 40,70);
+}
+
+function modelLoaded() {
+	console.log('Model Loaded!');
+}
+
+function gotPoses(results) {
+	if(results.length > 0)
+		{
+			console.log(results);
+			noseX = results[0].pose.nose.x;
+			noseY = results[0].pose.nose.y;
+			console.log("noseX = " + noseX +", noseY = " + noseY);
+		}
+}
+
+
+
+
+
